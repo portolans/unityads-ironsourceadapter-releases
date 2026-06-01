37c01200ae95c0ed81806aa86c3d67fedc9167e4
Portolabot <portolabot@hiportola.com>
2026-06-01 07:59:56 +0000

    Update to 5.8.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ISUnityAdsAdapter",
-            url: "https://github.com/portolans/unityads-ironsourceadapter-releases/releases/download/5.7.0/ISUnityAdsAdapter.xcframework.zip",
-            checksum: "890ca910dd5bc53873585ece0fae2ea95aec6cd8ee5bbec04f0fa5b95ed4dc83",
+            url: "https://github.com/portolans/unityads-ironsourceadapter-releases/releases/download/5.8.0/ISUnityAdsAdapter.xcframework.zip",
+            checksum: "e0d92c0b11597e38a76044165d6ff64ff525e0e5a4eea5d7137c3c6121b08f3b",
         ),
     ],
 )
